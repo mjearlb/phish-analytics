e0518d175a55e8220026081a918ad24f07c108c9
mjearlb <milobauman@gmail.com>
2023-11-30 15:48:31 -0500

    Added printing for the requested data

--- src/main/java/com/mjearlb/app/App.java
+++ src/main/java/com/mjearlb/app/App.java
@@ -1,6 +1,7 @@
 package com.mjearlb.app;
 
 import com.mjearlb.api.RequestPhishData;
+import com.mjearlb.api.RequestPhishData.PhishData;;
 
 /**
  * This app will allow for analysis of Phish.net
@@ -8,6 +9,15 @@ import com.mjearlb.api.RequestPhishData;
  */
 public class App {
     public static void main(String[] args) {
-        RequestPhishData.contactPhishNet("shows");
+        PhishData phishData = RequestPhishData.contactPhishNet("shows");
+        phishData.sortByShowDate(); 
+        printData(phishData); 
     } // main
+
+    /** 
+     * Prints the data we want printed to the terminal. 
+     */
+    public static void printData(PhishData phishData) {
+        System.out.print(phishData); 
+    } // printData
 } // App
